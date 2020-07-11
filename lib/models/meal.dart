4442515c7f93b092604d6571ae343b69e71e2ccc
Jason Tame <jason@advantagelearn.com>
2020-07-11 14:20:54 +0200

    Create foundations of meal model

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/foundation.dart';
 
+class Meal {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String hostName;
+  final String hostLocation;
+
+  Meal(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      this.price,
+      @required this.hostName,
+      @required this.hostLocation});
+}
