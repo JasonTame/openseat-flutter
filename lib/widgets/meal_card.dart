ecaf54852c2bd0de9306a35d6a9e3fb50e813418
Jason Tame <jason@advantagelearn.com>
2020-07-18 16:21:42 +0200

    Finish basic meal card example

--- lib/widgets/meal_card.dart
+++ lib/widgets/meal_card.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_card_header.dart';
+import '../widgets/meal_card_user_info.dart';
+import '../widgets/meal_card_description.dart';
+
 class MealCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-            child: Row(
-              children: <Widget>[Text('Event name'), Text('18 Jul')],
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            MealCardHeader(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: MealCardUserInfo(),
             ),
-          ),
-        ],
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: MealCardDescription()),
+          ],
+        ),
       ),
     );
   }
