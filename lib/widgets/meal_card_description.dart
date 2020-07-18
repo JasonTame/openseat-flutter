ac209b90a49c731a056eeac47b15933b9ff95b20
Jason Tame <jason@advantagelearn.com>
2020-07-18 16:21:23 +0200

    Add meal card description widget

---
+++ lib/widgets/meal_card_description.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MealCardDescription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          "Bobotie is a delightful blend of complex flavours. It is essentially a meatloaf that's mixed with...",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.grey.shade700),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: FlatButton(
+            onPressed: () {},
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.black87)),
+            child: Text(
+              'More Details',
+              style: TextStyle(color: Colors.grey.shade800),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
