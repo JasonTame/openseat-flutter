04cdd3c7f42105c4dc7e427a11da9425229ebcd7
Jason Tame <jason@advantagelearn.com>
2020-07-18 16:21:32 +0200

    Add meal card user info widget

---
+++ lib/widgets/meal_card_user_info.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MealCardUserInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        CircleAvatar(
+          backgroundColor: Colors.grey.shade900,
+          child: Text('SS'),
+          minRadius: 40.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'SIYA SHANGE',
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                ' Musgrave, Dbn',
+                textAlign: TextAlign.left,
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
