bd645b41f4581388036bc3c6349ff18fc3935e84
Jason Tame <jason@advantagelearn.com>
2020-09-06 15:46:18 +0200

    Wrap app in a change notifier provider and listen for events

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'app_scaffold.dart';
+
+import './providers/events.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Open Seat',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider(
+      create: (ctx) => Events(),
+      child: MaterialApp(
+        title: 'Open Seat',
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+          accentColor: Colors.amber,
+        ),
+        home: AppScaffold(),
+        routes: {},
       ),
-      home: AppScaffold(),
-      routes: {},
     );
   }
 }
